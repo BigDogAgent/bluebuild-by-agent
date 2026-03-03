857090240f993d5db54314cf1db7e18620ac52e8
bigdogagent <igor.kerstges@gmail.com>
2026-03-03 17:15:30 +0100

    add: excalidraw podman as local app

--- files/scripts/fix-permissions.sh
+++ files/scripts/fix-permissions.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 chmod +x /usr/libexec/igors-desktop-setup
 ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
+
+# Ensure the new Excalidraw files are globally readable
+chmod 644 /usr/lib/systemd/user/excalidraw.service
+chmod 644 /usr/share/applications/excalidraw.desktop
+chmod 644 /etc/xdg/kwinrulesrc
+chmod 644 /usr/share/icons/excalidraw.png
