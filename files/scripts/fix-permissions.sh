8b07cd2da7e4e0906ed575fe23f096322aed0fcc
BigDog <igor.kerstges@gmail.com>
2026-03-03 20:20:55 +0100

    fix: pull excaladraw image in first login, explicit call to docker.io

--- files/scripts/fix-permissions.sh
+++ files/scripts/fix-permissions.sh
@@ -3,6 +3,7 @@ chmod +x /usr/libexec/igors-desktop-setup
 ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
 
 # Ensure the new Excalidraw files are globally readable
+chmod 644 /usr/lib/systemd/user/podman-pull-excalidraw.service
 chmod 644 /usr/lib/systemd/user/excalidraw.service
 chmod 644 /usr/share/applications/excalidraw.desktop
 chmod 644 /etc/xdg/kwinrulesrc
