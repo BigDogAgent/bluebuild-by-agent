a30926b9a422a38a8d2443e41742450bd27e4726
IgorKerstges <ikerstges@gmail.com>
2026-03-04 14:58:13 +0100

    fix: 2nd attempt shutdownGracePeriod=0 (fix permissions)

--- files/scripts/fix-permissions.sh
+++ files/scripts/fix-permissions.sh
@@ -8,3 +8,6 @@ chmod 644 /usr/lib/systemd/user/excalidraw.service
 chmod 644 /usr/share/applications/excalidraw.desktop
 chmod 644 /etc/xdg/kwinrulesrc
 chmod 644 /usr/share/icons/excalidraw.png
+
+# Ensure the new shutdown/restart config files are globally readable
+chmod 644 /etc/xdg/ksmserverrc
